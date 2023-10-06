178a514c7abbfd41aee8c8a46d532a26b52f9800
unknown <ehsanullahhaidary@gmail.com>
2023-10-06 17:07:16 +0430

    mobile and pc setup done

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,34 @@
 import React from "react";
 import "./content.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Content({ openSidebar }) {
+  const [mobileSize, setMobileSize] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 500) {
+      if (openSidebar === true) {
+        setMobileSize(true);
+      } else {
+        setMobileSize(false);
+      }
+    } else {
+      setMobileSize(false);
+    }
+  }, [openSidebar]);
+  console.log(mobileSize);
   return (
-    <div className={openSidebar ? "content margin" : "content"}>Content</div>
+    <div
+      className={
+        mobileSize
+          ? "darken content"
+          : "content" && openSidebar
+          ? "content margin"
+          : "content"
+      }
+    >
+      ContentContentContentContentContentContentContent
+    </div>
   );
 }
 
